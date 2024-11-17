617f2497bf102612a785af6adb9ead05d7c770de
chengpeng-wcp <chengpeng.wcp@outlook.com>
2024-11-18 00:04:28 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
-#!/usr/bin/env sh
-
-# 确保脚本抛出遇到的错误
-set -e
-
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git push -f git@github.com:305810827/myDocs.git master:gh-pages
-
-cd -
-
+   #!/usr/bin/env sh
+   
+   # 当发生错误时中止脚本
+   set -e
+   
+   # 构建
+   npm run build
+   
+   # cd 到构建输出的目录下
+   cd dist
+   
+   # 部署到自定义域域名
+   # echo 'www.example.com' > CNAME
+   
+   git init
+   git add -A
+   git commit -m 'deploy'
+   
+   # 部署到 https://<USERNAME>.github.io
+   # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+   
+   # 部署到 https://<USERNAME>.github.io/<REPO>
+   # 将下面这行替换成自己的仓库地址
+   git push -f git@github.com/chengpeng-wcp/wcp-blog.git master:gh-pages   
+   cd -
