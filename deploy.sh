db7abaf6f14651eadb95c5ab1c8532bf6a3443a0
chengpeng-wcp <chengpeng.wcp@outlook.com>
2024-11-17 23:58:52 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/chengpeng-wcp/wcp-blog.git master:gh-pages
+git push -f git push -f git@github.com:305810827/myDocs.git master:gh-pages
 
 cd -
 
